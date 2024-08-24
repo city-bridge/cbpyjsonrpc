70331a2efae0e82d811084b73864af97afbe90f0
city-bridge <hironori0130@gmail.com>
2024-08-24 23:37:59 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='cbpyjsonrpc',
+    version='2024.8.15',
+    description='json-rpc server clinet',
+    url='https://github.com/city-bridge/cbpyjsonrpc',
+    packages=[''],
+    install_requires=[
+        # List your dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
